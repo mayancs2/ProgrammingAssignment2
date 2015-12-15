700fc4e197fb7fbed1d824f3880b2bf7a9125142
mayancs2 <mayancs2@gmail.com>
2015-12-15 23:27:20 +1100

    Matrix inversion with caching implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,77 @@
-## Put comments here that give an overall description of what your
-## functions do
+###############################################################################
+# Matrix inversion with caching
+# Caching based on makeVector/cachemean by rdpeng
+# Copyright (C) 2015  C.S.
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+###############################################################################
 
-## Write a short comment describing this function
-
+###############################################################################
+# Store a valid non-empty square matrix and 
+# Arguments - non-null, non-empty, square matrix
+# Returns list of methods to change it (set), retrieve it (get), 
+#     store inverse (setinverse), retrieve inverse (getinverse)
+#
+###############################################################################
 makeCacheMatrix <- function(x = matrix()) {
-
+        # Check if square matrix is specified        
+        if (is.null(x) || !is.matrix(x) || nrow(x) == 0 || nrow(x) != ncol(x)) {      
+                stop("Invalid or empty or non-square matrix specified")
+        }
+        m <- NULL
+        
+        set <- function(y) {
+                if (is.null(y) || !is.matrix(y) || nrow(y) != ncol(y)) { 
+                        stop("Invalid or non-square matrix specified")
+                }                
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+###############################################################################
+# Calculate the inverse of the stored matrix, cache it and return it. 
+# Directly return if already present in cache.
+# Assumes matrix has inverse
+# Arguments - list returned by makeCacheMatrix
+# Returns inverse of cached matrix
+# 
+###############################################################################
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        rows <- nrow(data)
+        
+        # make identity matrix
+        # identity <- matrix(rep(0, rows*rows), nrow=rows, ncol=rows)
+        # lapply(seq_len(rows), function(i) identity[i,i] <<- 1)
+        identity <- diag(rows)
+        
+        m <- solve(data, identity, ...)
+        x$setinverse(m)
+        m
 }
+
